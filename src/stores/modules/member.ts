1ac59fef9e2dcd017cb7f6dfa955cbc1c39b1312
loogeek <xiaolong3956@gmail.com>
2023-08-19 17:55:30 +0800

    init uni app project

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,24 +1,22 @@
-import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { LoginResult } from '@/types/member'
 
 // 定义 Store
 export const useMemberStore = defineStore(
   'member',
   () => {
     // 会员信息
-    const profile = ref<any>()
+    const profile = ref<LoginResult>()
 
-    // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
+    const setProfile = (val: LoginResult) => {
       profile.value = val
     }
 
-    // 清理会员信息，退出时使用
     const clearProfile = () => {
       profile.value = undefined
     }
 
-    // 记得 return
     return {
       profile,
       setProfile,
@@ -27,6 +25,15 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    persist: {
+      storage: {
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
   },
 )
